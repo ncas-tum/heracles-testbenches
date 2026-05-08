fix(CV): make the calculation select a proper slice
Previously the CV calculation was selecting voltage/current slices that
would not give a proper capacitance calculation depending on the
simulation.

testbenches/ngspice/cv.py
@@ -20,17 +20,17 @@ tsine = 1e-3
 splice = (tstop - tsine) / tstep
 
 fig, ax = plt.subplots(figsize=[3.45, 2.3])
+fig_raw, ax_raw = plt.subplots(nrows=2, sharex=True, figsize=[3.45, 2.3])
 for index_reset, vreset in enumerate(vreset_list):
     capacitances = []
 
     for index_set, vset in enumerate(vset_list):
-
         run = index_reset * len(vset_list) + (index_set + 1)
-        logger.info(f"Run: {run} {vreset} V -> {vset} V")
+        logger.info(f"Run: {run} {vreset:.4g} V -> {vset:.4g} V")
 
         circuit = Circuit("FeCap parameter extraction")
         circuit.raw_spice = f"""
-            .options reltol=0.001 abstol=1e-12 gmin=1e-12
+            .options reltol=0.001 abstol=1e-12 gmin=1e-12 method=Gear
 
             * Load OSDI model
             .control
@@ -38,31 +38,31 @@ for index_reset, vreset in enumerate(vreset_list):
             .endc
 
             * Model definition with current parameters
-            .model fecap heracles ( 
+            .model fecap heracles (
                 + area = 1e-12
-                + t_fe = 10e-9 
-                + t_int = 0.5e-9 
-                + eps_fe = 70 
+                + t_fe = 10e-9
+                + t_int = 0.5e-9
+                + eps_fe = 70
                 + eps_int = 7
-                + w_b = 1.05 
-                + d_e = 7.5e-9 
+                + w_b = 1.05
+                + d_e = 7.5e-9
                 + e_off = 1e7
-                + p_s = 20e-2 
-                + n_depl = 1.2e28 
+                + p_s = 20e-2
+                + n_depl = 1.2e28
                 + eps_depl = 2.2
-                + q_fix_depl_u = -8e-2 
-                + q_fix_depl_d = 20e-2 
-                + m_eff_fe = 0.4 
+                + q_fix_depl_u = -8e-2
+                + q_fix_depl_d = 20e-2
+                + m_eff_fe = 0.4
                 + m_eff_int = 0.3
-                + phi_b_fe = 2 
+                + phi_b_fe = 2
                 + phi_b_int = 2.7
                 + mu_fe = 0.2
-                + n_c_fe = 1e24 
-                + phi_tr_fe = 1.05 
-                + area_mc = 0 
-                + t_fe_mc = 0 
-                + t_int_mc = 0 
-                + p_s_mc = 0 
+                + n_c_fe = 1e24
+                + phi_tr_fe = 1.05
+                + area_mc = 0
+                + t_fe_mc = 0
+                + t_int_mc = 0
+                + p_s_mc = 0
                 + n_depl_mc = 0)
 
             * Circuit elements
@@ -73,28 +73,39 @@ for index_reset, vreset in enumerate(vreset_list):
             R2 be 0 50
 
             .probe I(V1)
+            .save vin @n1[c_depl_tot]
             """
 
         simulator = circuit.simulator(temperature=21, nominal_temperature=21)
         analysis = simulator.transient(tstep, tstop)
 
-        time = analysis.time[int(-splice*6/10):int(-splice/10)]
-        voltage = analysis.vin[int(-splice*6/10):int(-splice/10)]
-        current = analysis.v1[int(-splice*6/10):int(-splice/10)]
+        time = analysis.time
+        voltage = analysis.vin
+        current = analysis.v1
+        c_depl_tot = analysis["@n1[c_depl_tot]"]
 
-        vmax_index = np.argmax(voltage)
-        vmin_index = np.argmin(voltage)
-        imax_index = np.argmax(current)
-        imin_index = np.argmin(current)
+        index_lo = np.searchsorted(time, tsine + 1 / fsine)
+        index_hi = np.searchsorted(time, tsine + 2 / fsine)
+        vmax_index = np.argmax(voltage[index_lo:index_hi])
+        vmin_index = np.argmin(voltage[index_lo:index_hi])
+        imax_index = np.argmax(current[index_lo:index_hi])
+        imin_index = np.argmin(current[index_lo:index_hi])
 
-        v_amplitude = (voltage[vmax_index] - voltage[vmin_index]) / 2
-        i_amplitude = (current[imax_index] - current[imin_index]) / 2
+        ax_raw[0].plot(time[index_lo:index_hi], voltage[index_lo:index_hi])
+        ax_raw[1].plot(time[index_lo:index_hi], current[index_lo:index_hi])
+
+        v_amplitude = (
+            voltage[index_lo + vmax_index] - voltage[index_lo + vmin_index]
+        ) / 2
+        i_amplitude = (
+            current[index_lo + imax_index] - current[index_lo + imin_index]
+        ) / 2
 
         z_abs = float(v_amplitude / i_amplitude)
-        z_phase = float((time[vmax_index] - time[imax_index]) * fsine * 2*np.pi)
-        impedance = z_abs * np.exp(1j*z_phase)
+        z_phase = float((time[vmax_index] - time[imax_index]) * fsine * 2 * np.pi)
+        impedance = z_abs * np.exp(1j * z_phase)
 
-        c = -1/np.imag(impedance)/2/np.pi/fsine
+        c = -1 / np.imag(impedance) / 2 / np.pi / fsine
         capacitances.append(c)
 
     ax.plot(vset_list, capacitances)

testbenches/vacask/postproc.py
@@ -1,39 +1,61 @@
-from rawfile import rawread
-import matplotlib as mpl
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
+from rawfile import rawread
 from scipy.integrate import cumulative_trapezoid
 
 data_p = rawread("tran1.raw").get()
-data_c = rawread("tran2.raw").get()
+data_c = rawread("tran2.raw").get(sweeps=2)
 
 current_p = -data_p["vin:flow(br)"]
 voltage_p = data_p["vin"]
-time_p= data_p["time"]
+time_p = data_p["time"]
 polarization = cumulative_trapezoid(current_p, time_p)
 
-current_c = -data_c["vpulse:flow(br)"]
-voltage_c = data_c["vin"]
-time_c = data_c["time"]
-
-index = np.searchsorted(time_c, 1e-3)
-
 cmap = plt.get_cmap("Dark2")
-
 fig_p, (ax_iv, ax_pv) = plt.subplots(2, 1, sharex=True)
-fig_c, ax_vsine = plt.subplots()
-ax_isine = ax_vsine.twinx()
+fig_c, ax_c = plt.subplots()
 
+# vbias = np.linspace(-4, 4, 11)
+# vbias = np.concatenate([vbias, vbias])
+vbias = []
+capacitances = []
+fsine = 1e4
+
+for sweep in range(data_c.sweepGroups):
+    current_c = -data_c[sweep, "vpulse:flow(br)"]
+    voltage_c = data_c[sweep, "vin"]
+    time_c = data_c[sweep, "time"]
+    index = np.searchsorted(time_c, 1.0e-3 + 1 / fsine)
+    index_hi = np.searchsorted(time_c, 1.0e-3 + 2 / fsine)
+
+    vmax_index = np.argmax(voltage_c[index:index_hi])
+    vmin_index = np.argmin(voltage_c[index:index_hi])
+    imax_index = np.argmax(current_c[index:index_hi])
+    imin_index = np.argmin(current_c[index:index_hi])
+
+    v_amplitude = (voltage_c[index + vmax_index] - voltage_c[index + vmin_index]) / 2
+    i_amplitude = (current_c[index + imax_index] - current_c[index + imin_index]) / 2
+
+    z_abs = float(v_amplitude / i_amplitude)
+    z_phase = float(
+        (time_c[index + vmin_index] - time_c[index + imin_index]) * fsine * 2 * np.pi
+    )
+    impedance = z_abs * np.exp(1j * z_phase)
+
+    c = 1 / np.imag(impedance) / 2 / np.pi / fsine
+    capacitances.append(c)
+    vbias.append(data_c.sweepData(sweep)["set"])
+
+# ax_c.plot(time_c, voltage_c / v_amplitude)
+# ax_c.plot(time_c, current_c / i_amplitude)
 ax_iv.plot(voltage_p, current_p)
 ax_pv.plot(voltage_p[1:], polarization)
-ax_vsine.plot(time_c[index:], voltage_c[index:], c=cmap(0/6), label="V")
-ax_isine.plot(time_c[index:], current_c[index:], c=cmap(1/6), label="I")
+ax_c.plot(vbias, capacitances)
+# ax_c.set_ylim([-1.5, 1.5])
+# ax_c.set_xlim([1e-3, 1.2e-3])
 
-ax_vsine.legend()
-ax_isine.legend()
-ax_vsine.set_xlabel("Time [s]")
-ax_vsine.set_ylabel("Voltage [V]")
-ax_isine.set_ylabel("Current [A]")
+ax_c.set_xlabel("Voltage [V]")
+ax_c.set_ylabel("Capacitance [C]")
 
 fig_p.savefig("hysteresis_p.png", dpi=300)
 fig_c.savefig("hysteresis_c.png", dpi=300)